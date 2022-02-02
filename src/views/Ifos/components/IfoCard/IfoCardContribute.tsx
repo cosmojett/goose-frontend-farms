61d0f2dbd5924d8f751c1ddca20a178a7f402e2b
ermvrs <ermvrs@live.com>
2022-02-02 23:59:34 +0300

    ifo claim check

--- src/views/Ifos/components/IfoCard/IfoCardContribute.tsx
+++ src/views/Ifos/components/IfoCard/IfoCardContribute.tsx
@@ -4,10 +4,13 @@ import { useWallet } from '@binance-chain/bsc-use-wallet'
 import BigNumber from 'bignumber.js'
 import { Contract } from 'web3-eth-contract'
 import { useERC20 } from 'hooks/useContract'
+import { BSC_BLOCK_TIME } from 'config'
+import useBlock from 'hooks/useBlock'
 import { useIfoAllowance } from 'hooks/useAllowance'
 import { useIfoApprove } from 'hooks/useApprove'
 import { IfoStatus } from 'config/constants/types'
 import { getBalanceNumber } from 'utils/formatBalance'
+import getTimePeriods from 'utils/getTimePeriods'
 import LabelButton from './LabelButton'
 import ContributeModal from './ContributeModal'
 
@@ -32,8 +35,10 @@ const IfoCardContribute: React.FC<Props> = ({
 }) => {
   const [pendingTx, setPendingTx] = useState(false)
   const [offeringTokenBalance, setOfferingTokenBalance] = useState(new BigNumber(0))
-  const [userInfo, setUserInfo] = useState({ amount: 0, claimed: false })
-
+  const [userInfo, setUserInfo] = useState({ amount: 0, isClaimed: false })
+  const [claimBlock, setClaimBlock] = useState(0);
+  const [claimTime, setClaimTime] = useState(0);
+  const currentBlock = useBlock()
   const { account } = useWallet()
   const contractRaisingToken = useERC20(currencyAddress)
   const allowance = useIfoAllowance(contractRaisingToken, address, pendingTx)
@@ -46,15 +51,17 @@ const IfoCardContribute: React.FC<Props> = ({
     const fetch = async () => {
       const balance = new BigNumber(await contract.methods.getOfferingAmount(account).call())
       const userinfo = await contract.methods.userInfo(account).call()
-
+      const cb = await contract.methods.claimBlock().call();
       setUserInfo(userinfo)
       setOfferingTokenBalance(balance)
+      setClaimBlock(cb);
+      setClaimTime(cb > currentBlock ? (cb - currentBlock) * BSC_BLOCK_TIME : 0);
     }
 
     if (account) {
       fetch()
     }
-  }, [account, contract.methods, pendingTx])
+  }, [account, contract.methods, pendingTx, currentBlock])
 
   if (allowance === null) {
     return null
@@ -67,7 +74,7 @@ const IfoCardContribute: React.FC<Props> = ({
   }
   const isFinished = status === 'finished'
   const percentOfUserContribution = new BigNumber(userInfo.amount).div(raisingAmount).times(100)
-
+  const timeUntil = getTimePeriods(claimTime)
   if (allowance <= 0) {
     return (
       <Button
@@ -92,19 +99,22 @@ const IfoCardContribute: React.FC<Props> = ({
   return (
     <>
       <LabelButton
-        disabled={pendingTx || userInfo.claimed}
+        disabled={pendingTx || userInfo.isClaimed || claimBlock > currentBlock}
         buttonLabel={isFinished ? 'Claim' : 'Contribute'}
         label={isFinished ? 'Your tokens to claim' : `Your contribution (${currency})`}
         value={
           // eslint-disable-next-line no-nested-ternary
           isFinished
-            ? userInfo.claimed
+            ? userInfo.isClaimed
               ? 'Claimed'
               : getBalanceNumber(offeringTokenBalance, tokenDecimals).toFixed(4)
             : getBalanceNumber(new BigNumber(userInfo.amount)).toFixed(4)
         }
         onClick={isFinished ? claim : onPresentContributeModal}
       />
+      <Text fontSize="14px" color="textSubtle" style={isFinished ? {display : 'flex'} : { display : 'none'}}>
+        {currentBlock < claimBlock ? `You will be able to claim tokens after ${timeUntil.days}d, ${timeUntil.hours}h, ${timeUntil.minutes}m.` : ``}
+      </Text>
       <Text fontSize="14px" color="textSubtle">
         {isFinished
           ? `You'll be refunded any excess tokens when you claim`
