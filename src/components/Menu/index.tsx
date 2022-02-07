7a9ef20b15cf2d3d2d291bda8ce6c44d94396df0
Erim varış <ermvrs@live.com>
2022-02-07 14:28:49 +0300

    uikit

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -12,7 +12,7 @@ const Menu = (props) => {
   const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
   const cakePriceUsd = usePriceCakeBusd()
-
+// ss
   return (
     <UikitMenu
       account={account}
